b18b15914068b1881cfbfc93bab9468ce2557161
Vuong Vu Van (VN) <vuong.vv@homecredit.vn>
2021-01-22 11:11:30 +0700

    Init 3

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftPM_CustomCorner",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/SwiftPM_CustomCorner"),
         .testTarget(
             name: "SwiftPM_CustomCornerTests",
             dependencies: ["SwiftPM_CustomCorner"]),
